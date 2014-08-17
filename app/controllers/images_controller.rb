bde50680e37549ae7f849720e9ac5a041a32f0a1
Neodelf <neodelf@gmail.com>
2014-08-17 16:21:01 +0400

    delete images_controller.rb

--- app/controllers/images_controller.rb
+++
@@ -1,8 +0,0 @@
-class ImagesController < ApplicationController
-  def destroy
-    @image = Image.find(params[:id])
-    @publication = @image.publication
-    @image.destroy
-    redirect_to edit_admin_publication_path(@publication)
-  end
-end
