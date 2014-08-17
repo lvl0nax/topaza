af1d9ff2b7aec2c3e1abfe25fd057d36c00e4ff3
lvl0nax <lvl0nax@gmail.com>
2014-08-17 23:56:53 +0400

    adding backend for creation brides

--- app/controllers/brides_controller.rb
+++ app/controllers/brides_controller.rb
@@ -5,7 +5,18 @@ class BridesController < ApplicationController
   end
 
   def create
+    bride = Bride.new(bride_params)
+    if bride.save!
+      render json: {status: 1}, status: 200
+    else
+      render json: {status: 0}
+    end
+  end
 
+  private
+
+  def bride_params
+    params.require(:bride).permit(:name, :dress_name, :comment, :picture)
   end
 
 end
