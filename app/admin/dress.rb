3b72ca397c6b0e01d123bd6b59f9c1d847007911
Neodelf <neodelf@gmail.com>
2014-08-21 22:44:10 +0400

    refactoring. dont work show action /admin/images/:id

--- app/admin/dress.rb
+++ app/admin/dress.rb
@@ -1,6 +1,4 @@
 ActiveAdmin.register Dress do
-  permit_params(:dress => [:body, :title, :seo_title, :seo_description, :seo_keywords, :image => [:name], :dress_consist => [:material, :embroidery]])
-
   form :partial => 'admin/dresses/form'
 
   show do |dress|
@@ -30,40 +28,36 @@ ActiveAdmin.register Dress do
   end
 
   controller do
+    def permitted_params
+      params.permit dress: [:body, :title, :seo_title, :seo_description, :seo_keywords]
+    end
+
     def find_resource
       scoped_collection.friendly.find(params[:id])
     end
 
     def update
       @dress = Dress.friendly.find(params[:id])
-      params[:dress][:image][:name].each {|name| @dress.images << Image.create(name: name)} unless params[:dress][:image].blank?
+      params[:image][:name].each {|name| @dress.images << Image.create(name: name)} if params[:image].present?
       @dress.dress_consists.delete_all
       @dress.dress_consists << DressConsist.find(params[:dress][:dress_consist][:material].to_i)
       @dress.dress_consists << DressConsist.find(params[:dress][:dress_consist][:embroidery].to_i)
-      @dress.category_id = params[:dress][:category][:category_id].to_i
+      @dress.category = Category.find(params[:dress][:category_id].to_i)
       respond_to do |format|
-        if @dress.update_attributes(title: params[:dress][:title],
-                                          body: params[:dress][:body],
-                                          seo_description: params[:dress][:seo_description],
-                                          seo_title: params[:dress][:seo_title],
-                                          seo_keywords: params[:dress][:seo_keywords])
-          format.html { redirect_to admin_dresses_path, notice: 'Dress was successfully created.' }
+        if @dress.update_attributes(permitted_params[:dress])
+          format.html { redirect_to admin_dresses_path, notice: 'Dress was successfully updates.' }
         else
-          format.html { render action: 'new', notice: 'Dress not created.' }
+          format.html { render action: 'new', notice: 'Dress not updated.' }
         end
       end
     end
 
     def create
-      @dress = Dress.new(title: params[:dress][:title],
-                         body: params[:dress][:body],
-                         seo_description: params[:dress][:seo_description],
-                         seo_title: params[:dress][:seo_title],
-                         seo_keywords: params[:dress][:seo_keywords])
-      params[:dress][:image][:name].each {|name| @dress.images << Image.create(name: name)} unless params[:dress][:image].blank?
+      @dress = Dress.new(permitted_params[:dress])
+      params[:name].each {|name| @dress.images << Image.create(name: name)} if params[:image].present?
       @dress.dress_consists << DressConsist.find(params[:dress][:dress_consist][:material].to_i)
       @dress.dress_consists << DressConsist.find(params[:dress][:dress_consist][:embroidery].to_i)
-      @dress.category_id = params[:dress][:category][:category_id].to_i
+      @dress.category = Category.find(params[:dress][:category_id].to_i)
 
       respond_to do |format|
         if @dress.save
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def dress_select(f, property, dress)
-    f.select :material, options_from_collection_for_select(DressConsist.where(property: property), :id, :title, dress.dress_consists.where(property: property).first.id), :include_blank => false, label: false
+    f.select property.downcase.to_sym, options_from_collection_for_select(DressConsist.where(property: property), :id, :title, dress.dress_consists.where(property: property).first.id), :include_blank => false, label: false
+  end
+
+  def set_default(dress)
+    dress.dress_consists.where(property: property).first.id if dress.dress_consists.present?
   end
 end
