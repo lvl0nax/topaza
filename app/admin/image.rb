745a54900b2dc98b6003621af94cf7f432538f8a
Neodelf <neodelf@gmail.com>
2014-08-22 11:03:52 +0400

    added feature which delete images from form

--- app/admin/image.rb
+++ app/admin/image.rb
@@ -1,17 +1,19 @@
 ActiveAdmin.register Image do
 
-  controller do
-    def destroy
-      @image = Image.find(params[:id])
-      @publication = @image.publication
-      if @image.destroy
-        redirect_to edit_admin_publication_path(@publication), notice: 'Image was successfully destroyed'
-      else
-        redirect_to edit_admin_publication_path(@publication), notice: 'Image not destroyed'
-      end
+  show do |image|
+    row :name do
+      image_tag image.name.url
     end
-
   end
 
+  controller do
+    def destroy
+      @image = Image.find(params[:id].to_i)
+      model = @image.imageable_type.downcase.pluralize
+      object_model_id = @image.imageable_id
+      notice = @image.destroy ? 'Image was successfully destroyed' : 'Image not destroyed'
+      redirect_to "/admin/#{model}/#{object_model_id}/edit", notice: "#{notice}"
+    end
+  end
 
 end
