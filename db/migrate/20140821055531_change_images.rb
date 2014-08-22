c38b1cc3883f8ae2360e968bac5986ac8d0d8201
Neodelf <neodelf@gmail.com>
2014-08-22 11:06:24 +0400

    fix migrates

--- db/migrate/20140821055531_change_images.rb
+++ db/migrate/20140821055531_change_images.rb
@@ -1,6 +1,6 @@
 class ChangeImages < ActiveRecord::Migration
   def change
-    remove_columns :images, :publication_id, :dress_id
+    remove_column :images, :publication_id
     add_column :images, :imageable_id, :integer
     add_column :images, :imageable_type, :string
     add_index :images, :imageable_id
