5185f2be3aa317f1059bfd467f1a3e8477a9bfb2
lvl0nax <lvl0nax@gmail.com>
2014-09-23 21:54:15 +0700

    try to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,12 +22,6 @@ task :copy_database_config, roles => :app do
   run "cp #{db_config} #{release_path}/config/database.yml"
 end
 
-after "deploy:update_code", :copy_assets
-task :copy_database_config, roles => :app do
-  db_config = "#{shared_path}/config/database.yml"
-  run "cp #{db_config} #{release_path}/config/database.yml"
-end
-
 # В rails 3 по умолчанию включена функция assets pipelining,
 # которая позволяет значительно уменьшить размер статических
 # файлов css и js.
