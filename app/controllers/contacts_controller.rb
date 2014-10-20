95022407ae3186f7b67cb2a0e699439b2f5283a5
lvl0nax <lvl0nax@gmail.com>
2014-10-20 19:25:32 +0700

    change contact for whitecheicks

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,7 +3,7 @@ class ContactsController < ApplicationController
     @contacts = [{city: 'Москва', name: 'Салон &laquo;KATRIN&raquo;', address: 'ул.Поварская д.35/28', tel: '(495) 691-61-69', time: 'Пн-Вс: <b>11:00 - 21:00</b> (без выходных)', site: 'www.katrin.ru', text: 'Предварительная запись'},
                  {city: 'Москва', name: 'Салон &laquo;KATRIN&raquo;', address: 'ул.Забелина, д.1а', tel: '(495) 625-04-11', time: 'Пн-Вс: <b>11:00 - 20:00</b> (без выходных)', site: 'www.katrin.ru', text: 'Предварительная запись'},
                  {city: 'Москва', name: 'Салон &laquo;ANIYA&raquo;', address: 'ул.Остоженка, 20', tel: '(495) 637-40-16', time: 'Пн-Вс: <b>11:00 - 20:00</b> (без выходных)', site: 'www.aniya.ru', text: 'Предварительная запись'},
-                 {city: 'Москва', name: 'Креативный свадебный<br>шоу-рум &laquo;WHITE CHICKS&raquo;', address: 'ул.Бол.Новодмитровская, д.36/4 стр.2', tel: '8(499) 39-101-59', time: 'Пн-Вс: <b>10:00 - 20:00</b> (без выходных)', site: 'www.whitechicks.ru', text: 'Предварительная запись'},
+                 {city: 'Москва', name: 'Креативный свадебный<br>шоу-рум &laquo;WHITE CHICKS&raquo;', address: 'Ленинргадский проспект 32/2', tel: '8(499) 39-101-59', time: 'Пн-Вс: <b>10:00 - 20:00</b> (без выходных)', site: 'www.whitechicks.ru', text: 'Предварительная запись'},
                  {city: 'Новороссийск', name: 'Салон &laquo;TOPAZA PELLA&raquo;', tel: '(8617) 71 19 80', text: 'Предварительная запись'},
                  {city: 'Иркутск', name: 'Салон &laquo;Галинка&raquo;', tel: '(3952) 22 38 39', text: 'Предварительная запись'},
                  {city: 'Тольятти', name: 'Салон &laquo;ТРИУМФ&raquo;', address: 'ул.70 Лет Октября, 47', tel: '(8482) 76 20 03', text: 'Предварительная запись'},
