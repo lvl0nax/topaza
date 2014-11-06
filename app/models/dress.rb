6aa578ca4b600cd4f6d15588fc74e6170c79b436
lvl0nax <lvl0nax@gmail.com>
2014-11-06 23:05:02 +0300

    fixes for dress order

--- app/models/dress.rb
+++ app/models/dress.rb
@@ -3,6 +3,8 @@ class Dress < ActiveRecord::Base
   friendly_id :title, use: :slugged
   acts_as_list
 
+  default_scope { order(:position) }
+
   has_many :images, as: :imageable, dependent: :destroy
   has_and_belongs_to_many :dress_consists, dependent: :nullify
   belongs_to :category
