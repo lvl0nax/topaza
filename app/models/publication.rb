452ccdfa0d643ece438d5ed9f6f337d8e089bfb1
Neodelf <neodelf@gmail.com>
2014-11-19 19:19:02 +0300

    fixs to publication

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -5,8 +5,14 @@ class Publication < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
 
+  before_save :set_main_image
+
   has_many :images, as: :imageable, dependent: :destroy
 
   scope :next, ->(id){where('id > ?', id).first}
   scope :prev, ->(id){where('id < ?', id).first}
+
+  def set_main_image
+    images.first.update_attribute('main', true)
+  end
 end
