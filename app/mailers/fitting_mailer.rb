ae39ea58dfdefc75152de3a41256271a7d934bf2
lvl0nax <lvl0nax@gmail.com>
2014-12-08 23:16:20 +0300

    little fixes

--- app/mailers/fitting_mailer.rb
+++ app/mailers/fitting_mailer.rb
@@ -1,9 +1,9 @@
 class FittingMailer < ActionMailer::Base
-  default from: 'topaza-pella'
+  default from: 'noreply@topaza-pella.ru'
 
   def new_fitting_mail(options)
     @fitting = options
-    emails = 'lvl0nax@gmail.com,lvl0nax@yandex.com'
+    emails = 'lvl0nax@gmail.com,lvl0nax@yandex.com,utkin_@hotmail.com'
     # emails = "9856544@gmail.com,akvapolus@restsguide.ru,lider-aqva@metropost.ru"
     mail to: emails, subject: 'Примерка платья'
   end
