87bcbf2c62b0dab1730222cf501da922cc4c5162
lvl0nax <lvl0nax@gmail.com>
2014-12-09 12:13:46 +0300

    update mailer

--- app/mailers/fitting_mailer.rb
+++ app/mailers/fitting_mailer.rb
@@ -3,9 +3,9 @@ class FittingMailer < ActionMailer::Base
 
   def new_fitting_mail(options)
     @fitting = options
-    emails = 'lvl0nax@gmail.com,lvl0nax@yandex.com,utkin_@hotmail.com'
+    emails = 'lvl0nax@gmail.com,kolibri@topaza-pella.ru,utkin_@hotmail.com'
     # emails = "9856544@gmail.com,akvapolus@restsguide.ru,lider-aqva@metropost.ru"
-    mail to: emails, subject: 'Примерка платья'
+    mail to: emails, subject: 'Запись на примерку'
   end
 
 end
