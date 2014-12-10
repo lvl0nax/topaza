4693726da2ca9e2f5202ed1e5ade8efbaadcfcfe
lvl0nax <lvl0nax@gmail.com>
2014-12-10 12:30:23 +0300

    update order for publications

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,11 +1,11 @@
 class PublicationsController < ApplicationController
   def index
-    @publications = Publication.order(:id).paginate(:page => params[:page], :per_page => 6)
+    @publications = Publication.order(id: :desc).paginate(:page => params[:page], :per_page => 6)
   end
 
   def show
     @publication = Publication.friendly.find(params[:id])
-    publications = Publication.order(:created_at)
+    publications = Publication.order(id: :desc)
     current_index = publications.index(@publication)
     @prev_publication = current_index-1 >= 0 ? publications[current_index-1] : nil
     @next_publication = current_index+1 <= publications.length ? publications[current_index+1] : nil
